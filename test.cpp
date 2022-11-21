77963c4e7c975c66436852f2015036de75b2150e
Serz999 <serz_999@mail.ru>
2022-11-21 10:21:45 +0300

    unicode test2

--- test.cpp
+++ test.cpp
@@ -6,12 +6,12 @@ std::string init_data;
 std::string unicode_init_data;
 
 TEST(fileload, initial_test){
-    InFileSearcher elem_finder("tmp_data.txt");
+    InFileSearcher elem_finder("data.tmp");
     ASSERT_EQ(init_data, elem_finder.GetFileData());
 }
 
 TEST(searching, async_mode){
-    InFileSearcher elem_finder("tmp_data.txt");
+    InFileSearcher elem_finder("data.tmp");
     std::vector<size_t> positions = elem_finder.Find("word");
     elem_finder.PrintFindedPositions(positions);
     std::vector<size_t> assert_positions = {0, 16, 30, 35, 47};
@@ -19,7 +19,7 @@ TEST(searching, async_mode){
 }
 
 TEST(searching, sync_mode){
-    InFileSearcher elem_finder("tmp_data.txt");
+    InFileSearcher elem_finder("data.tmp");
     std::vector<size_t> positions = elem_finder.Find("sync", "word");
     elem_finder.PrintFindedPositions(positions);
     std::vector<size_t> assert_positions = {0, 16, 30, 35, 47};
@@ -27,7 +27,7 @@ TEST(searching, sync_mode){
 }
 
 TEST(warnings, unknown_mode){
-    InFileSearcher elem_finder("tmp_data.txt");
+    InFileSearcher elem_finder("data.tmp");
     std::vector<size_t> positions = elem_finder.Find("special_mode", "word");
     elem_finder.PrintFindedPositions(positions);
     std::vector<size_t> assert_positions = {0, 16, 30, 35, 47};
@@ -35,7 +35,7 @@ TEST(warnings, unknown_mode){
 }
 
 TEST(searching, unicode_handling){
-    InFileSearcher elem_finder("tmp_unicodedata.txt");
+    InFileSearcher elem_finder("unicode_data.tmp");
     std::vector<size_t> positions = elem_finder.Find("мир!");
     elem_finder.PrintFindedPositions(positions);
     std::vector<size_t> assert_positions = {13, 51};
@@ -51,10 +51,10 @@ TEST(exceptions, filepath_error){
 int main(int argc, char **argv) {
     init_data = "word aasdasdasd word asdasdas word word awdawd word";
     unicode_init_data = "привет мир! прощай жестокий мир!";
-    std::ofstream ofs("tmp_data.txt");
+    std::ofstream ofs("data.tmp");
     ofs << init_data;
     ofs.close();
-    ofs.open("tmp_unicodedata.txt");
+    ofs.open("unicode_data.tmp");
     ofs << unicode_init_data;
     ofs.close();
     ::testing::InitGoogleTest(&argc, argv);
