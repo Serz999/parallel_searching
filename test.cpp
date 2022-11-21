8f4dbbe20746ec86309620b44f50a5046a78b527
Serz999 <serz_999@mail.ru>
2022-11-21 10:19:35 +0300

    unicode test

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 std::string init_data;
+std::string unicode_init_data;
 
 TEST(fileload, initial_test){
     InFileSearcher elem_finder("tmp_data.txt");
@@ -33,6 +34,14 @@ TEST(warnings, unknown_mode){
     ASSERT_EQ(positions, assert_positions);
 }
 
+TEST(searching, unicode_handling){
+    InFileSearcher elem_finder("tmp_unicodedata.txt");
+    std::vector<size_t> positions = elem_finder.Find("мир!");
+    elem_finder.PrintFindedPositions(positions);
+    std::vector<size_t> assert_positions = {13, 51};
+    ASSERT_EQ(positions, assert_positions);
+}
+
 TEST(exceptions, filepath_error){
     ASSERT_ANY_THROW({
         InFileSearcher elem_finder("incorrect_filepath");
@@ -41,8 +50,12 @@ TEST(exceptions, filepath_error){
 
 int main(int argc, char **argv) {
     init_data = "word aasdasdasd word asdasdas word word awdawd word";
+    unicode_init_data = "привет мир! прощай жестокий мир!";
     std::ofstream ofs("tmp_data.txt");
     ofs << init_data;
+    ofs.close();
+    ofs.open("tmp_unicodedata.txt");
+    ofs << unicode_init_data;
     ofs.close();
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
